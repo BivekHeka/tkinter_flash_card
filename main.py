097b4937ffb314974c72a84d4d5188468929dca3
Bivek Heka <bevillheka@gmail.com>
2026-02-13 23:52:43 +0545

    csv connected and card flipped after 3 sec timer

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#b1ddc6"
 
+data = pandas.read_csv("D:/Python/flash_card/data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+current_card = {}
+
+
+
+def next_card():
+    global current_card, flip_timer   # <-- add flip_timer here
+    window.after_cancel(flip_timer)   # cancel previous timer
+    current_card = random.choice(to_learn)
+    current_card["French"]
+    canvas.itemconfig(card_title, text = "French")
+    canvas.itemconfig(card_word, text = current_card["French"] )
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_background, image = card_back_img )
+    canvas.itemconfig(card_title, text = "English")
+    canvas.itemconfig(card_word, text = current_card["English"])
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(0, lambda: None)  # dummy initial timer
+
 canvas = Canvas(width=800,height=526)
 card_front_img = PhotoImage(file="D:/Python/flash_card/images/card_front.png")
-canvas.create_image(400,263,image = card_front_img)
-canvas.create_text(400,150, text="Title", font=("arial",40,"italic"))
-canvas.create_text(400,263 ,text="word" , font=("arial",60,"bold"))
+card_back_img = PhotoImage(file="D:/Python/flash_card/images/card_back.png")
+card_background = canvas.create_image(400,263,image = card_front_img)
+card_title = canvas.create_text(400,150, text="", font=("arial",40,"italic"))
+card_word = canvas.create_text(400,263 ,text="" , font=("arial",60,"bold"))
 canvas.config(bg=BACKGROUND_COLOR , highlightthickness=0)
 canvas.grid(row=0,column=0,columnspan=2)
 
 cross_image = PhotoImage(file="D:/Python/flash_card/images/wrong.png")
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1,column=0)
-unknown_button.config(highlightthickness=0)
+
 
 check_image = PhotoImage(file="D:/Python/flash_card/images/right.png")
-known_button = Button(image=check_image,highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0 ,command=next_card)
 known_button.grid(row=1,column=1)
+
+next_card()
 
 
 window.mainloop()
