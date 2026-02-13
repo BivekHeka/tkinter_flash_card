f809ec08ed12fecd6df7b1130b0d293ec7ce3e1b
Bivek Heka <bevillheka@gmail.com>
2026-02-14 00:16:48 +0545

    UI fiexed after card flip

--- main.py
+++ main.py
@@ -17,15 +17,17 @@ def next_card():
     window.after_cancel(flip_timer)   # cancel previous timer
     current_card = random.choice(to_learn)
     current_card["French"]
-    canvas.itemconfig(card_title, text = "French")
-    canvas.itemconfig(card_word, text = current_card["French"] )
+    canvas.itemconfig(card_title, text = "French", fill="black")
+    canvas.itemconfig(card_word, text = current_card["French"], fill="black" )
     flip_timer = window.after(3000, flip_card)
+    canvas.itemconfig(card_background, image = card_front_img)
+
 
 
 def flip_card():
     canvas.itemconfig(card_background, image = card_back_img )
-    canvas.itemconfig(card_title, text = "English")
-    canvas.itemconfig(card_word, text = current_card["English"])
+    canvas.itemconfig(card_title, text = "English", fill="white")
+    canvas.itemconfig(card_word, text = current_card["English"], fill="white")
 
 window = Tk()
 window.title("Flashy")
